debebfddb8346406d0404051074c7d72fc12acd2
usuario <usuario@10.22.143.7>
2014-10-14 14:18:44 +0100

    Aplication ready v1

--- src/frecuencia/MailReader.java
+++ src/frecuencia/MailReader.java
@@ -1,7 +1,34 @@
 package frecuencia;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class MailReader {
     
+    private final String filepath;
+
+    public MailReader(String filepath) {
+        this.filepath = filepath;
+    }
     
+    public String[] readDomains() {
+        ArrayList<String> domainsList = new ArrayList<>();
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(filepath));
+            while (true) {
+                String line = reader.readLine();
+                if (line == null) break;
+                if (line.contains("@")) domainsList.add(line.split("@")[1]);
+            }
+        } catch (IOException ex) {
+            Logger.getLogger(MailReader.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        return domainsList.toArray(new String[domainsList.size()]);
+    }
     
 }
--- src/frecuencia/Main.java
+++ src/frecuencia/Main.java
@@ -4,7 +4,11 @@ public class Main {
     
     public static void main(String[] args) {
         
-        HistogramBuilder<String> builder;
+        MailReader reader = new MailReader("mails.txt");
+        HistogramBuilder<String> builder = new HistogramBuilder<>();
+        Histogram<String> histogram = builder.build(reader.readDomains());
+        
+        new HistogramView<String>().print(histogram);
         
     }
     
